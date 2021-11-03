cbe774cc84011296d83f72f33b462f4e75cd3f84
pdeshp8 <pdeshp8@uic.edu>
2021-11-03 01:39:42 -0500

    - Implemented application.conf - Added System.exit in main

--- build.sbt
+++ build.sbt
@@ -6,9 +6,12 @@ scalaVersion := "2.12.4"
 
 val akkaVersion = "2.5.26"
 val akkaHttpVersion = "10.1.11"
+val typesafeConfigVersion = "1.4.1"
+
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-stream" % akkaVersion,
   "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
   "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion,
+  "com.typesafe" % "config" % typesafeConfigVersion,
 )
--- src/main/scala/com/parth/client/client.scala
+++ src/main/scala/com/parth/client/client.scala
@@ -1,28 +1,34 @@
 package com.parth.client
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.client.RequestBuilding.Get
-//import akka.actor.TypedActor.dispatcher
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpMethods, HttpResponse, HttpRequest}
+import akka.http.scaladsl.client.RequestBuilding.Get
+import akka.http.scaladsl.model.{HttpEntity, HttpResponse}
 import akka.stream.ActorMaterializer
+import com.typesafe.config.{Config, ConfigFactory}
 
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
-import scala.concurrent.Future
 
 object client {
   implicit val system: ActorSystem = ActorSystem()
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   import system.dispatcher
+  val config: Config = ConfigFactory.load()
+  val range: Int = config.getInt("parameters.range")
+  val time: String = config.getString("parameters.time")
+  val bucket: String = config.getString("parameters.bucket")
+  val key: String = config.getString("parameters.key")
+  val timeout: FiniteDuration = config.getInt("parameters.timeout").seconds
 
   def sendRequest(): Future[String] = {
-//    val responseFuture: Future[HttpResponse] = Http().singleRequest(request)
-    val responseFuture: Future[HttpResponse] = Http().singleRequest(Get("https://a94he0mou3.execute-api.us-east-2.amazonaws.com/default/bucketextract?range=5&time=09:34:09.000&bucket=logsinput&key=log.log"))
-    val entityFuture: Future[HttpEntity.Strict] = responseFuture.flatMap(response => response.entity.toStrict(2.seconds))
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(Get(s"https://a94he0mou3.execute-api.us-east-2.amazonaws.com/default/bucketextract?range=$range&time=$time&bucket=$bucket&key=$key"))
+    val entityFuture: Future[HttpEntity.Strict] = responseFuture.flatMap(response => response.entity.toStrict(timeout))
     entityFuture.map(entity => entity.data.utf8String)
   }
 
   def main(args: Array[String]): Unit ={
-    sendRequest().foreach(println )
+    System.out.println(Await.result(sendRequest(), timeout))
+    System.exit(0)
   }
 }
